e15671fda1e3b57af35c33917026fd2890af9bc1
jcsii98 <jose.c.saribong@gmail.com>
2023-09-25 21:33:36 +0800

    initial auth setup

---
+++ src/components/AuthPage.jsx
@@ -0,0 +1,131 @@
+import dog1 from "../assets/dog1.jpg";
+import React, { useState } from "react";
+export default function AuthPage() {
+  const [toggleAuth, setToggleAuth] = useState(false);
+
+  const handleToggleAuth = () => {
+    setToggleAuth((prevToggle) => !prevToggle);
+  };
+  return (
+    <>
+      <div
+        id="main-container"
+        className="bg-white rounded-lg flex overflow-hidden h-[645px] w-[875px]"
+      >
+        <div
+          id="container-left"
+          className="flex-1 bg-cover bg-center"
+          style={{ backgroundImage: `url(${dog1})` }}
+        ></div>
+        <div id="container-right" className="flex-1 flex flex-col px-10">
+          <div className="text-sm font-bold py-6">
+            <p className="cursor-pointer">Go Back</p>
+          </div>
+          <div className="text-3xl font-medium pb-6">
+            {toggleAuth ? (
+              <>Sign up</>
+            ) : (
+              <>Sign in with your email or username</>
+            )}
+          </div>
+          {toggleAuth ? (
+            <>
+              <div className="">
+                <form className="flex flex-col">
+                  <div className="pb-4">
+                    <div className="font-medium pb-2 text-slate-700">Email</div>
+                    <input
+                      type="text"
+                      className="focus:outline-none focus:border-slate-600 border-[1px] border-slate-400 rounded-md w-full py-2 px-2"
+                    ></input>
+                  </div>
+                  <div className="pb-4">
+                    <div className="font-medium pb-2 text-slate-700">
+                      Username
+                    </div>
+                    <input
+                      type="text"
+                      className="focus:outline-none focus:border-slate-600 border-[1px] border-slate-400 rounded-md w-full py-2 px-2"
+                    ></input>
+                  </div>
+                  <div className="pb-4">
+                    <div className="font-medium pb-2 text-slate-700">
+                      Password
+                    </div>
+                    <input
+                      type="text"
+                      className="focus:outline-none focus:border-slate-600 border-[1px] border-slate-400 rounded-md w-full py-2 px-2"
+                    ></input>
+                  </div>
+                  <div className="pb-4">
+                    <div className="font-medium pb-2 text-slate-700">
+                      Confirm Password
+                    </div>
+                    <input
+                      type="text"
+                      className="focus:outline-none focus:border-slate-600 border-[1px] border-slate-400 rounded-md w-full py-2 px-2"
+                    ></input>
+                  </div>
+                  <div className="pt-2 w-full">
+                    <button
+                      className="w-full hover:bg-slate-400 hover:border-[#00000000] hover:text-white text-black py-2 my-2 rounded-lg border-black border-[1px] px-4"
+                      type="submit"
+                    >
+                      Sign Up
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="">
+                <form className="flex flex-col">
+                  <div className="pb-4">
+                    <div className="font-medium pb-2 text-slate-700">
+                      Email or Username
+                    </div>
+                    <input
+                      type="text"
+                      className="focus:outline-none focus:border-slate-600 border-[1px] border-slate-400 rounded-md w-full py-2 px-2"
+                    ></input>
+                  </div>
+                  <div className="pb-4">
+                    <div className="font-medium pb-2 text-slate-700">
+                      Password
+                    </div>
+                    <input
+                      type="text"
+                      className="focus:outline-none focus:border-slate-600 border-[1px] border-slate-400 rounded-md w-full py-2 px-2"
+                    ></input>
+                  </div>
+                  <div className="pt-2 w-full">
+                    <button
+                      className="w-full hover:bg-slate-400 hover:border-[#00000000] hover:text-white text-black py-2 my-2 rounded-lg border-black border-[1px] px-4"
+                      type="submit"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
+          )}
+
+          <div className="text-sm">
+            <div className="flex">
+              Or {toggleAuth ? <>sign in</> : <>sign up</>}{" "}
+              <div
+                onClick={handleToggleAuth}
+                className="cursor-pointer pl-[4px] underline"
+              >
+                here
+              </div>
+              .
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,30 @@
+import AuthPage from "../components/authPage";
 export default function Root() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">I am Root</h1>
+      {/* <div className="bg-slate-400 w-screen h-screen flex flex-col justify-between px-6">
+        <div className="h-[10%]"></div>
+        <div className="">
+          <div className="py-4 flex justify-center text-2xl">
+            <div className="font-light">Pack</div>
+            <div className="font-normal">Walk</div>
+          </div>
+          <div className="flex flex-col">
+            <button className="hover:bg-white hover:shadow-black hover:text-black text-white py-2 my-2 rounded-lg border-white border-2 text-xl">
+              Login
+            </button>
+            <button className="hover:bg-white hover:shadow-black hover:text-black text-white py-2 my-2 rounded-lg border-white border-2 text-xl">
+              Sign Up
+            </button>
+          </div>
+        </div>
+        <div className="h-[10%]"></div>
+      </div> */}
+      <div className="bg-slate-400 w-screen h-screen overflow-hidden">
+        <div className="w-full h-full flex justify-center items-center">
+          <AuthPage />
+        </div>
+      </div>
     </>
   );
 }
