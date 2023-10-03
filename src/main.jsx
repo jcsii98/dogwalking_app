b963ec236f68108bb21896d65194693dcba71a30
jcsii98 <jose.c.saribong@gmail.com>
2023-10-04 01:41:33 +0800

    route for confirmed

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
     path: "/admin",
     element: <AdminRoot />,
   },
+  { path: "/confirmed", element: <Root /> },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
